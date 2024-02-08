93c46e89b90003cd48b8cda4493fb4db252ea8e3
Marouane Boufarouj <104838272+grainme@users.noreply.github.com>
2024-02-08 14:27:07 +0100

    Update Developer.tsx

--- src/pages/Developer.tsx
+++ src/pages/Developer.tsx
@@ -5,12 +5,12 @@ function Dev() {
   return (
     <div className="flex flex-col justify-between items-center min-h-[103vh] sm:min-h-[100vh] bg-black text-white font-CD">
       <NavBar />
-      <div className="sm:w-[50%] flex flex-col justify-center items-center">
-        <h1 className="text-[40px] sm:text-[100px] mb-[-1rem] sm:mb-[-2rem] ">
+      <div className="sm:w-[70%] flex flex-col justify-center items-center">
+        <h1 className="text-[20px] sm:text-[40px] mb-[-1rem] sm:mb-[-2rem] ">
           Marouane Boufarouj & Ismail Assou
         </h1>
         <div className="text-[12px] sm:text-[14px] text-gray-300 font-GN">
-          aka Marouane Boufarouj, CS Student from Morocco!
+          Students from ENSA Khouribga, Morocco!
         </div>
       </div>
       <Card />
