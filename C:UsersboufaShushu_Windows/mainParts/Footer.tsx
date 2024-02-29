4d93562499d505c7d05eb8af1a6cea503bc8f4f1
Marouane Boufarouj <104838272+grainme@users.noreply.github.com>
2024-02-29 22:39:54 +0100

    Delete C:UsersboufaShushu_Windows directory

--- C:UsersboufaShushu_Windows/mainParts/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import { Card } from "./popper";
-
-export function Footer() {
-  return (
-    <div className="flex flex-row text-slate-200 w-screen justify-center items-center gap-6 text-[13px] mb-5">
-      <Card />
-    </div>
-  );
-}
