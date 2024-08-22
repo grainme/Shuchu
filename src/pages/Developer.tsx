f6ba8b28b9b75ccabf470752d59aa11de47d5a86
Marouane Boufarouj <104838272+grainme@users.noreply.github.com>
2024-08-22 23:56:14 +0100

    Update Developer.tsx

--- src/pages/Developer.tsx
+++ src/pages/Developer.tsx
@@ -6,7 +6,7 @@ function Dev() {
     <div className="flex flex-col justify-between items-center min-h-[103vh] sm:min-h-[100vh] bg-black text-white font-CD">
       <NavBar />
       <div className="sm:w-[70%] flex flex-col justify-center items-center">
-        <h1 className="text-[20px] sm:text-[40px] mb-[-1rem] sm:mb-[-2rem] ">
+        <h1 className="text-[10px] sm:text-[40px] mb-[-1rem] sm:mb-[-2rem] ">
           dev by Marouane Boufarouj
         </h1>
       </div>
